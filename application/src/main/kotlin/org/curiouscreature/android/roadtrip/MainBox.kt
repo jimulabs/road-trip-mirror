5aeebd16088a6e63de7a8245736659fef414d349
Linton Ye <linton@jimulabs.com>
2015-03-06 15:51:13 -0800

    Update to use the latest mirror-sandbox

--- application/src/main/kotlin/org/curiouscreature/android/roadtrip/MainBox.kt
+++ application/src/main/kotlin/org/curiouscreature/android/roadtrip/MainBox.kt
@@ -3,20 +3,14 @@ package org.curiouscreature.android.roadtrip
 import android.view.View
 
 import com.jimulabs.mirrorsandbox.MirrorSandbox
+import com.jimulabs.mirrorsandbox.MirrorSandboxBase
 
 /**
  * Created by lintonye on 15-02-21.
  */
-public class MainBox(private val mRootView: View) : MirrorSandbox {
-
-    {
-        val introView = mRootView.findViewById(R.id.intro) as IntroView
+public class MainBox(private val rootView: View) : MirrorSandboxBase(rootView) {
+    override fun `$onLayoutDone`(rootView: View) {
+        val introView = rootView.findViewById(R.id.intro) as IntroView
         introView.setSvgResource(R.raw.map_usa)
-    }
-
-    override fun enterSandbox() {
-    }
-
-    override fun destroySandbox() {
     }
 }
